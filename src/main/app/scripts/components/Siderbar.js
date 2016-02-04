01fedf7245fe5cefc589d433149d9181d14fa4f0
ondrejhudek <ondrej.hudek@gmail.com>
2016-02-04 23:29:25 +0100

    New features

--- src/main/app/scripts/components/Siderbar.js
+++ src/main/app/scripts/components/Siderbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { LeftNav } from 'material-ui'
+import FontAwesome from 'react-fontawesome'
 
 import Menu from '../components/Menu'
 
@@ -14,11 +15,26 @@ class Sidebar extends React.Component {
 
     render() {
         return (
-            <LeftNav open={this.state.open} className="sidebar">
+            <LeftNav open={this.state.open} className="sidebar" width={265}>
                 <header>
-                    <h1>hudyapp <small>beta</small></h1>
+                    <h1>
+                        <FontAwesome name="hand-peace-o"/> hudyapp
+                        <small>beta</small>
+                    </h1>
                 </header>
+
                 <Menu/>
+
+                <footer>
+                    <p className="copyright">
+                        &copy; 2016 Made with <FontAwesome name="heart"/> by Ondřej Hudek
+                    </p>
+                    <p className="github">
+                        <a href="https://github.com/ondrejhudek/hudy-app">
+                            <FontAwesome name="github" size="3x"/>
+                        </a>
+                    </p>
+                </footer>
             </LeftNav>
         )
     }
